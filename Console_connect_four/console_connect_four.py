8bc524144c74cc21fd4577c44ef3b4ff926e962e
VasilMG <vassil.manolov@abv.bg>
2023-07-08 03:15:54 +0300

    change connect four file

--- Console_connect_four/console_connect_four.py
+++ Console_connect_four/console_connect_four.py
@@ -1,38 +1,96 @@
-def get_initial_field(rows_count, columns_count):
-    return [[None] * columns_count for _ in range(rows_count)]
-def print_field(field):
-    [print([x if x else 0 for x in row]) for row in field]
+def create_matrix(rows: int, columns: int):
+    matrix = []
+    for _ in range(rows):
+        matrix.append([0] * columns)
 
-def get_player_move(player): 
-    player_move_str = input(f' Player {player} please choose a column.')
-    player_move = int(player_move_str)
-    return player_move - 1 
+    return matrix
 
-def apply_player_move_genn(max_row, column_count): 
-    free_buttom_row_indeces = [max_row] * column_count 
-    def apply_player_move(player, player_move, field): 
-        player_row = free_buttom_row_indeces[player_move] 
-        field[player_row][player_move] = player
-        print(free_buttom_row_indeces)
-        free_buttom_row_indeces[player_move] -= 1
-    return apply_player_move 
-                          
-def is_win_condition(field):
-    pass
 
-def play(field):
-    row_count = len(field)
-    col_count = len(field[0])
-    apply_player_move = apply_player_move_genn(row_count, col_count)
-    current_player, other_player = 1, 2
+def player_choice(player):
+    choice = int(input(f'Player {player}, please choose a column:\n'))
+
+    return choice - 1
+
+
+def implement_player_data_to_main_logic(playground, player_index, player):
+    start_row_index = 0
+
+    while start_row_index < len(playground) and playground[start_row_index][player_index] == 0:
+        start_row_index += 1
+
+    playground[start_row_index - 1][player_index] = player
+
+    return start_row_index - 1, player_index
+
+
+def is_win_condition(player, player_row, player_column, playground, win_counter):
+
+    def convert_player_position_in_direction(playground, initial_row, initial_column, row_change, column_change):
+        row, column = initial_row, initial_column
+        row_change *= -1
+        column_change *= -1
+
+        while 0 <= row < len(playground) and 0 <= column < len(playground[0]):
+            if playground[row][column] != player:
+                break
+            column += column_change
+            row += row_change
+
+        if row == initial_row and column == initial_column:
+            return row, column
+
+        return row - row_change, column - column_change
+
+    def is_win_condition_in_direction(playground, initial_row, initial_column, direction, win_counter):
+        row_change, column_change = direction
+        row, column = convert_player_position_in_direction(playground, initial_row, initial_column, row_change, column_change)
+        rows_border = min(len(playground), row + win_counter * row_change)
+        columns_border = min(len(playground[0]), column + win_counter * column_change)
+        row_included_condition = rows_border == row
+
+        counter = 0
+
+        while (row != rows_border or row_included_condition) and (column != columns_border or (columns_border == column)) \
+                and player == playground[row][column]:
+            counter += 1
+            row += row_change
+            column += column_change
+
+        return counter == win_counter
+
+    directions = [(0, 1), (1, 0), (1, 1), (-1, 1)]
+
+    for direction in directions:
+        if is_win_condition_in_direction(playground, player_row, player_column, direction, win_counter):
+            return True
+    else:
+        return False
+
+def game_play_function(playground, win_counter):
+    current_player, second_player = 1, 2
+
     while True:
-        player_move = get_player_move(current_player)
-        apply_player_move(current_player, player_move, field)
-        if in_win_condition(field):
-            break
-        else:
-            current_player, other_player = other_player, current_player
-            print_winner(field)
+        current_player_choice = player_choice(current_player)
+        row_index, column_index = implement_player_data_to_main_logic(playground, current_player_choice, current_player)
+        print_function(playground)
 
-field = get_initial_field(5, 7)
-play(field)
+        if is_win_condition(current_player, row_index, column_index, playground, win_counter):
+            print(f'The winner is player {current_player}')
+            break
+
+        current_player, second_player = second_player, current_player
+
+
+def print_function(playground):
+    for row in playground:
+        print(row)
+
+
+try:
+    rows = int(input('Enter the number of rows: '))
+    columns = int(input('Enter the number of columns: '))
+    win_counter = int(input('Enter the number of win combination: '))
+    playground = create_matrix(rows, columns)
+    game_play_function(playground, win_counter)
+except ValueError:
+    print('Invalid values, try again!')
